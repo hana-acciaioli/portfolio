721c70ea4a54a538f9c28c37a8128dab33101591
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-02-22 19:00:29 -0800

    header displaying with full and max media sizes in place

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,5 +2,17 @@ import React from 'react';
 import './Header.css';
 
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="container-fluid">
+        <div className="row me animated fadeIn">
+          <div className="col-xs-offset-5 col-sm-offset-6 col-xs-7 col-sm-6">
+            <div className="tagline2 animated fadeIn">HANA </div>
+            <div className="tagline3 animated fadeIn">ACCIAIOLI </div>
+            <div className="tagline1">Full-Stack Software Engineer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
